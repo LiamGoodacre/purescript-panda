7d0136a309b0ab96f4bfb344203419a3e3367bd5
Liam Goodacre <goodacre.liam@gmail.com>
2018-01-20 02:31:56 +0000

    Mess with everything

--- src/Panda/Types.purs
+++ src/Panda/Types.purs
@@ -3,40 +3,63 @@ module Panda.Application where
 -- There are problems to be had with cyclical dependencies. In an effort to
 -- avoid them, this file declares all the relevant types in one go.
 
-import Data.Lens    (Lens')
-import FRP.Event    (Event)
-import Util.Exists2 (Exists2)
+import Prelude
+import Data.Profunctor (dimap, rmap)
+import Data.Lens (ALens)
+import FRP.Event (Event)
+import Util.Exists2 (Exists4, runExists4, mkExists4)
+import Data.Exists (Exists, mkExists, runExists)
 
 
 -- Configuration for a Panda application. These three values encompass
 -- everything required for a Panda application to be fully functioning.
 
-type Application event state
-  = ∀ action
+newtype Application' si so ei eo action
+  = Application'
+      { -- A view is the generated output. Certain components within it can be
+        -- configured to listen for a particular event and, with that and the
+        -- state, update themselves. Moreover, UI events can trigger _actions_,
+        view ∷ si → Component si so ei eo action
 
-  . { -- A view is the generated output. Certain components within it can be
-      -- configured to listen for a particular event and, with that and the
-      -- state, update themselves. Moreover, UI events can trigger _actions_,
-      view ∷ state → Component event state action
+        -- A UI can be affected by an event (which itself can be the combination
+        -- of many events) that occurs according to some side-effects.
+      , subscription ∷ Event eo
 
-      -- A UI can be affected by an event (which itself can be the combination
-      -- of many events) that occurs according to some side-effects.
-    , subscription ∷ Event event
+        -- When a UI action occurs, this function produces the UI update event.
+      , update ∷ si → action → { event ∷ eo, state ∷ so }
 
-      -- When a UI action occurs, this function produces the UI update event.
-    , update ∷ state → action → { event ∷ event, state ∷ state }
+      }
 
-    }
+
+newtype Application si so ei eo
+  = Application (Exists (Application' si so ei eo))
+
+mkApplication
+  ∷ ∀ si so ei eo action
+  . Application' si so ei eo action
+  → Application si so ei eo
+
+mkApplication app = Application (mkExists app)
+
+runApplication
+  ∷ ∀ si so ei eo r
+  . (∀ action. Application' si so ei eo action -> r)
+  → Application si so ei eo
+  → r
+
+runApplication f (Application app)
+  = runExists f app
+
 
 
 -- A regular element - one with nothing interesting going on - is just a tag
 -- name, some properties, and any children underneath. Of course, not all
 -- elements will have children.
 
-type Element event state action
+type Element si so ei eo action
   = { tagName    ∷ String
     , properties ∷ Array (Property action)
-    , children   ∷ Array (Component event state action)
+    , children   ∷ Array (Component si so ei eo action)
     }
 
 
@@ -44,9 +67,9 @@ type Element event state action
 -- predicate passes, the render function is called with the current state to
 -- determine what needs to be done with the DOM element. It's quite nifty.
 
-type Watcher event state action
-  = { interest ∷ event → Boolean
-    , renderer ∷ state → Component event state action
+type Watcher si so ei eo action
+  = { interest ∷ ei → Boolean
+    , renderer ∷ Component si so ei eo action
     }
 
 
@@ -54,49 +77,182 @@ type Watcher event state action
 -- neat because we can write applications that focus on specific parts of our
 -- data, and then compose them together to deal with larger structures.
 
-newtype Delegate' event state subevent substate
+newtype Delegate' si so ei eo xsi xso xei xeo
   = Delegate'
       { focus
-          ∷ { state ∷ Lens' state subevent
-            , event ∷ Lens' event subevent
+          ∷ { state ∷ ALens si so xsi xso
+            , event ∷ ALens ei eo xei xeo
             }
 
-      , delegate ∷ Application subevent substate
+      , delegate ∷ Application xsi xso xei xeo
       }
 
 
-type Delegate event state = Exists2 (Delegate' event state)
+type Delegate si so ei eo = Exists4 (Delegate' si so ei eo)
 
 
 -- A component is just the stuff from above. :)
 
-data Component event state action
-  = Element  (Element  event state action)
-  | Watcher  (Watcher  event state action)
-  | Delegate (Delegate event state)
-
+data Component si so ei eo action
+  = Element  (Element  si so ei eo action)
+  | Watcher  (Watcher  si so ei eo action)
+  | Delegate (Delegate si so ei eo)
+  | Text     String
 
 -- TODO: expand this out _a lot_.
 
-type Property action
-  = { key ∷ String
-    , value ∷ String
-    }
+data Property action
+  = Property
+      { key ∷ String
+      , value ∷ String
+      }
+  | OnClick action -- TODO
 
+derive instance functorProperty :: Functor Property
 
 -- Neat little fold.
 
 component
-  ∷ ∀ eff event state action result
-  . (Element   event state action → result) 
-  → (Watcher   event state action → result)
-  → (Delegate  event state        → result)
-  →  Component event state action
+  ∷ ∀ si so ei eo action result
+  . (Element   si so ei eo action → result) 
+  → (Watcher   si so ei eo action → result)
+  → (Delegate  si so ei eo        → result)
+  → (String                       → result)
+  →  Component si so ei eo action
   → result
 
-component element watcher delegate
+component element watcher delegate text
   = case _ of
       Element  value → element  value
       Watcher  value → watcher  value
       Delegate value → delegate value
+      Text     value → text     value
+
+varyApplication
+  ∷ ∀ si si' so so' ei ei' eo eo'
+  . (si' → si)
+  → (so → so')
+  → (ei' → ei)
+  → (eo -> eo')
+  → Application si so ei eo
+  → Application si' so' ei' eo'
+
+varyApplication fsi fso fei feo app
+  = runApplication onApp app
+  where
+
+    onApp
+      ∷ ∀ action
+      . Application' si so ei eo action
+      → Application si' so' ei' eo'
+
+    onApp (Application' {view, subscription, update})
+      = mkApplication $ Application'
+          { view: dimap fsi (varyComponent fsi fso fei feo) view
+          , subscription: feo <$> subscription
+          , update: dimap fsi (rmap varyUpdateCod) update
+          }
+
+    varyUpdateCod {event, state}
+      = { event: feo event
+        , state: fso state
+        }
+
+
+varyComponent
+  ∷ ∀ si si' so so' ei ei' eo eo' action
+  . (si' → si)
+  → (so → so')
+  → (ei' → ei)
+  → (eo → eo')
+  → Component si so ei eo action
+  → Component si' so' ei' eo' action
+
+varyComponent fsi fso fei feo = go where
+  go = case _ of
+    Element {tagName, properties, children} →
+      Element { tagName
+              , properties
+              , children: go <$> children
+              }
+
+    Watcher {interest, renderer} →
+      Watcher { interest: interest <<< fei
+              , renderer: go renderer
+              }
+
+    Delegate d →
+      (runExists4 onDelegate d)
+
+    Text s ->
+      Text s
+
+  onDelegate
+    ∷ forall a b c d
+    . Delegate' si so ei eo a b c d
+    → Component si' so' ei' eo' action
+
+  onDelegate (Delegate' {focus: {state, event}, delegate})
+    = Delegate (mkExists4 (Delegate' value))
+    where
+      value = { focus:
+                  { state: rmap (dimap fsi fso) state
+                  , event: rmap (dimap fei feo) event
+                  }
+
+              , delegate
+              }
+
+
+mkElement
+  ∷ ∀ si so ei eo action
+  . String
+  → Array (Property action)
+  → Array (Component si so ei eo action)
+  → Component si so ei eo action
+
+mkElement tagName properties children = Element
+  { tagName
+  , properties
+  , children
+  }
+
+mkButton
+  ∷ ∀ si so ei eo action
+  . Array (Property action)
+  → Array (Component si so ei eo action)
+  → Component si so ei eo action
+
+mkButton = mkElement "button"
+
+mkText
+  ∷ ∀ si so ei eo action
+  . String
+  → Component si so ei eo action
+
+mkText = Text
+
+data Act = Noop | Inc
+
+foo :: Application Int Int Boolean Boolean
+foo = mkApplication $ Application'
+  let view i =
+        mkButton
+          [ OnClick Inc
+          ]
+
+          [ mkText (show i)
+          ]
+
+      subscription = pure false
+
+      update state = case _ of
+        Noop -> { event: false, state }
+        Inc -> { event: true, state: state + 1 }
+
+  in { view
+     , subscription
+     , update
+     }
+
 
--- src/Util/Exists2.purs
+++ src/Util/Exists2.purs
@@ -2,6 +2,9 @@ module Util.Exists2
   ( Exists2
   , mkExists2
   , runExists2
+  , Exists4
+  , mkExists4
+  , runExists4
   ) where
 
 import Unsafe.Coerce (unsafeCoerce)
@@ -16,4 +19,13 @@ mkExists2 = unsafeCoerce
 
 runExists2 ∷ ∀ f r. (∀ x y. f x y → r) → Exists2 f → r
 runExists2 = unsafeCoerce
+
+
+foreign import data Exists4 ∷ (Type -> Type -> Type -> Type -> Type) -> Type
+
+mkExists4 ∷ ∀ f a b c d. f a b c d -> Exists4 f
+mkExists4 = unsafeCoerce
+
+runExists4 ∷ ∀ f r. (∀ a b c d. f a b c d -> r) -> Exists4 f -> r
+runExists4 = unsafeCoerce
 
